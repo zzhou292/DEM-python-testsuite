ad65cecb29faba54cf5a192cbad5752da8a742c9
zzhou292 <zzhou292@wisc.edu>
2021-12-15 13:41:12 -0600

    Update damping

--- dem_sys.py
+++ dem_sys.py
@@ -1,5 +1,22 @@
 import csv
 import math
+def vec3dot(vec1, vec2):
+      return vec1[0]*vec2[0]+vec1[1]*vec2[1]+vec1[2]*vec2[2]
+
+def vec3minus(vec1,vec2):
+      res = []
+      res.append(vec1[0]-vec2[0])
+      res.append(vec1[1]-vec2[1])
+      res.append(vec1[2]-vec2[2])
+      return res
+
+def vec3add(vec1,vec2):
+      res=[]
+      res.append(vec1[0]+vec2[0])
+      res.append(vec1[1]+vec2[1])
+      res.append(vec1[2]+vec2[2])
+      return res
+
 class DEMSystem:
       def __init__(self, pos, vel, fix):
         self.pos_list = pos
@@ -15,20 +32,39 @@ class DEMSystem:
 
       def set_collision_factor(self, stiff, damp):
             self.stiffness = stiff
-            self.damping = damp
+            self.restitution_coe = damp
 
 
-      def collision_handler(self, dir_x, dir_y, dir_z):
-        penetration = math.sqrt(dir_x*dir_x + dir_y*dir_y + dir_z*dir_z)
+      def collision_handler(self, dir, vel_1, vel_2):
+        # stiffness
+        penetration = math.sqrt(dir[0]*dir[0]+dir[1]*dir[1]+dir[2]*dir[2])
         frc_mag = penetration * self.stiffness
-        x_ratio = dir_x / math.sqrt(dir_x*dir_x + dir_y*dir_y + dir_z*dir_z)
-        y_ratio = dir_y / math.sqrt(dir_x*dir_x + dir_y*dir_y + dir_z*dir_z)
-        z_ratio = dir_z / math.sqrt(dir_x*dir_x + dir_y*dir_y + dir_z*dir_z)
-        frc = []
-        frc.append(frc_mag*(x_ratio))
-        frc.append(frc_mag*(y_ratio))
-        frc.append(frc_mag*(z_ratio))
-        return frc
+        x_ratio = dir[0] / penetration
+        y_ratio = dir[1] / penetration
+        z_ratio = dir[2] / penetration
+        frc_stiff = []
+        frc_stiff.append(frc_mag*(x_ratio))
+        frc_stiff.append(frc_mag*(y_ratio))
+        frc_stiff.append(frc_mag*(z_ratio))
+
+        # damping
+        M  = (self.mass*self.mass)/(self.mass+self.mass);
+        K  = self.stiffness;
+        C  = 10.*(1./math.sqrt(1. + math.pow(math.pi/math.log(self.restitution_coe), 2)))*math.sqrt(K*M)
+        normal = []
+        normal.append(dir[0]*x_ratio)
+        normal.append(dir[1]*y_ratio)
+        normal.append(dir[2]*z_ratio)
+        V  = vec3dot(vec3minus(vel_2, vel_1), normal)
+        frc_damp = []
+        frc_damp.append(C*V*normal[0])
+        frc_damp.append(C*V*normal[1])
+        frc_damp.append(C*V*normal[2])
+        #print("frc_stiff: "+str(frc_stiff))
+        #print("frc_damp-: "+str(frc_damp))
+
+        
+        return vec3add(frc_stiff,frc_damp)
 
       def forward(self,timestep):
             n = len(pos_list)
@@ -41,17 +77,18 @@ class DEMSystem:
               acc_ele.append(0)
               acc.append(acc_ele)
 
-            
+            # handle collision
             for i in range(n):
                   for j in range(n):
                         if i != j:
                               pos1 = pos_list[i]
                               pos2 = pos_list[j]
-                              dir_x = pos1[0] - pos2[0]
-                              dir_y = pos1[1] - pos2[1]
-                              dir_z = pos1[2] - pos2[2]
-                              if dir_x*dir_x+dir_y*dir_y+dir_z*dir_z < (self.radius*2)*(self.radius*2):
-                                frc = self.collision_handler(dir_x, dir_y, dir_z)
+                              dir = []
+                              dir.append(pos1[0] - pos2[0])
+                              dir.append(pos1[1] - pos2[1])
+                              dir.append(pos1[2] - pos2[2])
+                              if dir[0]*dir[0]+dir[1]*dir[1]+dir[2]*dir[2] < (self.radius*2)*(self.radius*2):
+                                frc = self.collision_handler(dir, self.vel_list[i],self.vel_list[j])
                                 if self.fix_list[i]==False:
                                   acc[i][0] += frc[0] / self.mass
                                   acc[i][1] += frc[1] / self.mass
@@ -60,6 +97,9 @@ class DEMSystem:
                                   acc[j][0] += - frc[0] / self.mass
                                   acc[j][1] += - frc[1] / self.mass
                                   acc[j][2] += - frc[2] / self.mass
+
+            # handle bounding conditions
+            
             
             for i in range(n):
               # gravity
@@ -100,7 +140,7 @@ for k in range (0,2,1):
             pos = []
             pos.append(-2+i*(radius*2+tol))
             pos.append(-2+j*(radius*2+tol))
-            pos.append(0 + k * (radius*2+tol))
+            pos.append(0 + k * (radius*2+3*tol))
             pos_list.append(pos)
 
             vel = []
@@ -115,11 +155,12 @@ for k in range (0,2,1):
               fix_list.append(False)
 
 dem1 = DEMSystem(pos_list,vel_list, fix_list)
-dem1.enforce_init_para(domain_x, domain_y, domain_z, radius , 1)
-dem1.set_collision_factor(200, 100)
+dem1.enforce_init_para(domain_x, domain_y, domain_z, radius , 4)
+dem1.set_collision_factor(150, 1e8)
 
-for i in range(100):
-  dem1.forward(0.01)
+for i in range(2000):
+  print(i)
+  dem1.forward(0.0025)
   dem1.csv_output("fld/test"+str(i))
 
 
