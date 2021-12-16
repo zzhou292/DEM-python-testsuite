080a44bdca841db12e9df670e1ff620ec4e68ae4
zzhou292 <zzhou292@wisc.edu>
2021-12-16 01:52:30 -0600

    add ml results

--- dem_sys.py
+++ dem_sys.py
@@ -1,5 +1,15 @@
 import csv
 import math
+import os
+
+
+def maximum(a, b):
+     
+    if a >= b:
+        return [a,0]
+    else:
+        return [b,1]
+
 def vec3dot(vec1, vec2):
       return vec1[0]*vec2[0]+vec1[1]*vec2[1]+vec1[2]*vec2[2]
 
@@ -22,6 +32,7 @@ class DEMSystem:
         self.pos_list = pos
         self.vel_list = vel
         self.fix_list = fix
+        self.col_out = False
 
       def enforce_init_para(self, x, y, z, r, m):
         self.domain_x = x
@@ -30,18 +41,26 @@ class DEMSystem:
         self.radius = r
         self.mass = m
 
-      def set_collision_factor(self, stiff, damp):
+      def set_collision_factor(self, stiff, damp, w_stiff):
             self.stiffness = stiff
-            self.restitution_coe = damp
-
+            self.damp = damp
+            self.w_stiff = w_stiff
+            #print("stiff:"+str(stiff))
+            #print("damp:"+str(damp))
+            #print("w_stiff:"+str(w_stiff))
+        
+      def set_col_out(self, write):
+          self.col_out = write
 
       def collision_handler(self, dir, vel_1, vel_2):
         # stiffness
-        penetration = math.sqrt(dir[0]*dir[0]+dir[1]*dir[1]+dir[2]*dir[2])
+        dist = math.sqrt(dir[0]*dir[0]+dir[1]*dir[1]+dir[2]*dir[2])
+        penetration = 2*self.radius - dist
+        #print("penetration: "+str(penetration))
         frc_mag = penetration * self.stiffness
-        x_ratio = dir[0] / penetration
-        y_ratio = dir[1] / penetration
-        z_ratio = dir[2] / penetration
+        x_ratio = dir[0] / dist
+        y_ratio = dir[1] / dist
+        z_ratio = dir[2] / dist
         frc_stiff = []
         frc_stiff.append(frc_mag*(x_ratio))
         frc_stiff.append(frc_mag*(y_ratio))
@@ -50,7 +69,7 @@ class DEMSystem:
         # damping
         M  = (self.mass*self.mass)/(self.mass+self.mass);
         K  = self.stiffness;
-        C  = 10.*(1./math.sqrt(1. + math.pow(math.pi/math.log(self.restitution_coe), 2)))*math.sqrt(K*M)
+        C  = 2.*(1./math.sqrt(1. + math.pow(math.pi/math.log(self.damp), 2)))*math.sqrt(K*M)
         normal = []
         normal.append(dir[0]*x_ratio)
         normal.append(dir[1]*y_ratio)
@@ -67,9 +86,18 @@ class DEMSystem:
         return vec3add(frc_stiff,frc_damp)
 
       def forward(self,timestep):
-            n = len(pos_list)
+            n = len(self.pos_list)
 
             acc = []
+            
+            
+            self.dir_store = []
+            self.vel1_store = []
+            self.vel2_store = []
+            self.frc_store = []
+                
+            
+            
             for i in range(n):
               acc_ele = []
               acc_ele.append(0)
@@ -97,25 +125,69 @@ class DEMSystem:
                                   acc[j][0] += - frc[0] / self.mass
                                   acc[j][1] += - frc[1] / self.mass
                                   acc[j][2] += - frc[2] / self.mass
-
+                                  
+                                if self.col_out == True:
+                                    self.dir_store.append(dir)
+                                    self.vel1_store.append(self.vel_list[i])
+                                    self.vel2_store.append(self.vel_list[j])
+                                    self.frc_store.append(frc)
+                                
+                
+                                    
             # handle bounding conditions
             for i in range(n):
-              pos = pos_list[i]
-              hit_bd = False
-              if abs(pos[0])>=domain_x/2:
-                hit_bd = True
-              elif abs(pos[1])>=domain_y/2:
-                hit_bd = True
-              elif abs(pos[2])>=domain_z/2:
-                hit_bd = True
+              if self.fix_list[i] == False:
+                  pos = self.pos_list[i]
+                  x_hit_bd = False
+                  y_hit_bd = False
+                  z_hit_bd = False
+                  
+                  x_pene = 0
+                  y_pene = 0
+                  z_pene = 0
+ 
+                  
+                  x_check = maximum(abs(pos[0]+radius),abs(pos[0]-radius))
+                  y_check = maximum(abs(pos[1]+radius),abs(pos[1]-radius))
+                  z_check = maximum(abs(pos[2]+radius),abs(pos[2]-radius))
+                  
+                  if x_check[0]>=domain_x/2:
+                    x_hit_bd = True
+                  elif y_check[0]>=domain_y/2:
+                    y_hit_bd = True
+                  elif z_check[0]>=domain_z/2:
+                    z_hit_bd = True
 
             
-              if hit_bd == True:
-                vel_list[i][0] = - vel_list[i][0] * 0.8 
-                vel_list[i][1] = - vel_list[i][1] * 0.8 
-                vel_list[i][2] = - vel_list[i][2] * 0.8 
+                  if x_hit_bd == True:
+                    if x_check[1]==0:
+                        x_frc = (abs(pos[0]+radius) - domain_x/2)*self.w_stiff
+                        acc[i][0] = acc[i][0]-x_frc/self.mass
+                    elif x_check[1]==1:
+                        x_frc = (abs(pos[0]-radius) - domain_x/2)*self.w_stiff
+                        acc[i][0] = acc[i][0]+x_frc/self.mass 
+                        
+                        
+                        
+                  if y_hit_bd == True:
+                    if y_check[1]==0:
+                        y_frc = (abs(pos[1]+radius) - domain_y/2)*self.w_stiff
+                        acc[i][1] = acc[i][1]-y_frc/self.mass
+                    elif y_check[1]==1:
+                        y_frc = (abs(pos[1]-radius) - domain_y/2)*self.w_stiff
+                        acc[i][1] = acc[i][1]+y_frc/self.mass  
+                    
+                    
+                  if z_hit_bd == True:
+                    #if z_check[1]==0:
+                        #z_frc = (abs(pos[2]+radius) - domain_z/2)*self.w_stiff
+                        #acc[i][2] = acc[i][2]-z_frc/self.mass
+                    if z_check[1]==1:
+                        z_frc = (abs(pos[2]-radius) - domain_z/2)*self.w_stiff
+                        acc[i][2] = acc[i][2]+z_frc/self.mass  
             
             
+            # gravity and integration
             for i in range(n):
               # gravity
               if self.fix_list[i]==False:
@@ -136,8 +208,48 @@ class DEMSystem:
             for i in range(n):
                 csvwriter.writerow([str(self.pos_list[i][0]) + ',' + str(self.pos_list[i][1]) + ',' + str(self.pos_list[i][2]) + ',' + str(self.vel_list[i][0])+
                 ',' + str(self.vel_list[i][1]) + ',' + str(self.vel_list[i][2])  ])
+                
+      def col_output(self,filename):
+          with open(filename+'.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+            csvwriter.writerow(['dir_x'+','+'dir_y'+','+'dir_z'+','+'vel1_x'+','+'vel1_y'+','+'vel1_z'+','+'vel2_x'+','+
+                                'vel2_y'+','+'vel2_z'+','+'frc_x'+','+'frc_y'+','+'frc_z'])
+            n = len(self.dir_store)
+            for i in range(n):
+                csvwriter.writerow([str(self.dir_store[i][0]) + ',' + str(self.dir_store[i][1]) + ',' + str(self.dir_store[i][2]) + ',' 
+                                    + str(self.vel1_store[i][0])+',' + str(self.vel1_store[i][1]) + ',' + str(self.vel1_store[i][2]) + ','
+                                    + str(self.vel2_store[i][0]) + ','+ str(self.vel2_store[i][1])+','+str(self.vel2_store[i][2]) + ','
+                                    + str(self.frc_store[i][0]) + ','+ str(self.frc_store[i][1])+','+str(self.frc_store[i][2])])
+                
+      def sys_ke(self):
+          n = len(self.pos_list)
+          sum = 0
+          for i in range(n):
+              sum = sum + 0.5 * self.mass * (self.vel_list[i][0] * self.vel_list[i][0] + 
+                                             self.vel_list[i][1] * self.vel_list[i][1] +
+                                             self.vel_list[i][2] * self.vel_list[i][2])
+          return sum
 
 
+
+# You should change 'test' to your preferred folder.
+MYDIR = ("fld")
+MYDIR1 = ("col")
+CHECK_FOLDER = os.path.isdir(MYDIR)
+CHECK_FOLDER1 = os.path.isdir(MYDIR1)
+
+# If folder doesn't exist, then create it.
+if not CHECK_FOLDER:
+    os.makedirs(MYDIR)
+    print("created folder : ", MYDIR)
+else:
+    print(MYDIR, "folder already exists.")
+    
+if not CHECK_FOLDER1:
+    os.makedirs(MYDIR1)
+    print("created folder : ", MYDIR1)
+else:
+    print(MYDIR1, "folder already exists.")
 
 domain_x = 6
 domain_y = 6
@@ -148,14 +260,22 @@ pos_list = []
 vel_list = []
 fix_list = []
 
-num = int(domain_x / (2 * radius + tol))
-for k in range (0,2,1):
+num = int((domain_x-2) / (2 * radius + tol))
+for k in range (0,4,1):
     for j in range(0, num, 1):
         for i in range(0, num, 1):
             pos = []
-            pos.append(-2+i*(radius*2+tol))
-            pos.append(-2+j*(radius*2+tol))
-            pos.append(0 + k * (radius*2+3*tol))
+            if k%2 == 0:
+                pos.append(-domain_x/2+1+i*(radius*2+tol))
+            else:
+                pos.append(-domain_x/2+1+i*(radius*2+tol)+tol)
+                
+            if k%2 == 0:
+                pos.append(-domain_y/2+1+j*(radius*2+tol))
+            else:
+                pos.append(-domain_y/2+1+j*(radius*2+tol)+tol)
+                
+            pos.append(-domain_z/2 + (k+1) * (radius*2+3*tol))
             pos_list.append(pos)
 
             vel = []
@@ -168,12 +288,24 @@ for k in range (0,2,1):
 
 
 dem1 = DEMSystem(pos_list,vel_list, fix_list)
-dem1.enforce_init_para(domain_x, domain_y, domain_z, radius , 4)
-dem1.set_collision_factor(150, 1e8)
+density = 4000
+mass = radius*radius*radius*4/3*math.pi*density
+dem1.enforce_init_para(domain_x, domain_y, domain_z, radius , mass)
+dem1.set_collision_factor(1e5, 1e8, 1e6)
+dem1.set_col_out(True)
 
-for i in range(1000):
-  print(i)
-  dem1.forward(0.005)
-  dem1.csv_output("fld/test"+str(i))
+write_skip = 50
+write_count = 0
+
+for i in range(40000):
+  ke = dem1.sys_ke()
+  print(str(i) + " ke: "+str(ke) )
+  dem1.forward(0.0005)
+  if i%write_skip == 0:
+      dem1.csv_output("fld/test"+str(write_count))
+      dem1.col_output("col/test"+str(write_count))
+     #dem1.print_col_pair("col/test"+str(write_count))
+      write_count=write_count+1
+      
 
 
